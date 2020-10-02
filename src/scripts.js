5eec9b76fd710370019b17e18b7211e28e8dce53
Gleb Varganov <webgleb@gmail.com>
2020-10-02 16:50:04 +0500

    Add link to own image

--- src/scripts.js
+++ src/scripts.js
@@ -1,5 +1,9 @@
 $( document ).ready(function() {
 
+	function checkURL(url) {
+		return(url.match(/\.(jpeg|jpg|gif|png)$/) != null);
+	}
+
 	$( "button.change-filter" ).on( "click", function() {
 		var imgClass = $( this ).attr( "data-filter" ),
 		filterDescription = $( this ).attr( "data-description" );
@@ -15,4 +19,56 @@ $( document ).ready(function() {
 		$('#img-src').attr('src', imgUrl);
 	});
 	
+	$( "button.own-image" ).on( "click", function() {
+
+		$.confirm({
+		    title: 'Своя картинка!',
+		    content: '' +
+		    '<form action="" class="formName">' +
+		    '<div class="form-group">' +
+		    '<label>Введите URL собственного изображения</label>' +
+		    '<input type="url" placeholder="Адрес картинки" class="image-url form-control" required />' +
+		    '</div>' +
+		    '</form>',
+		    buttons: {
+		        formSubmit: {
+		            text: 'Изменить',
+		            btnClass: 'btn-blue',
+		            action: function () {
+		                var urlImg = this.$content.find('.image-url').val();
+		                urlImg = urlImg.trim();
+		                var checkImage = checkURL( urlImg );
+		                
+		                if(!urlImg) {
+		                    $.alert('Введите ссылку на изображение!');
+		                    return false;
+		                }
+
+		                if(!checkImage) {
+		                    $.alert('Введите корректную ссылку на изображение!');
+		                    return false;
+		                }
+
+		                $('#img-src').attr('src', urlImg);
+
+		            }
+		        },
+		        cancel: function () {
+		            //close
+		        },
+		    },
+		    onContentReady: function () {
+		        // bind to events
+		        var jc = this;
+		        this.$content.find('form').on('submit', function (e) {
+		            // if the user submits the form by pressing enter in the field.
+		            e.preventDefault();
+		            jc.$$formSubmit.trigger('click'); // reference the button and click it
+		        });
+		    }
+		});
+
+
+	});
+
 });
